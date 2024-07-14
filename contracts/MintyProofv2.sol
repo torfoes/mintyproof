d71c507ad78898d3788b31148fda2b2351efbaa3
torfoes <karloszuru@gmail.com>
2024-07-14 08:05:40 +0200

    contracts

--- contracts/MintyProofv2.sol
+++ contracts/MintyProofv2.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 
 contract VisitProofNFT is ERC721Enumerable, Ownable {
     using ECDSA for bytes32;
@@ -21,6 +20,7 @@ contract VisitProofNFT is ERC721Enumerable, Ownable {
         Ownable(initialOwner)
     {
     }
+
     event LogEthSignedMessageHash(bytes32 indexed ethSignedMessageHash);
 
     function registerIssuer(bytes32 issuerId, bytes memory publicKey, uint256 nftTokenId) public {
@@ -42,15 +42,15 @@ contract VisitProofNFT is ERC721Enumerable, Ownable {
 
         emit LogEthSignedMessageHash(ethSignedMessageHash);
 
-
+        // Extract the address from the public key
         address issuerAddress = address(uint160(uint256(keccak256(issuer.publicKey))));
 
-        require(
-            SignatureChecker.isValidSignatureNow(issuerAddress, ethSignedMessageHash, issuerSignature),
-            "Invalid signature"
-        );
+        // Recover the signer's address from the signature
+        address recoveredAddress = ecrecover(ethSignedMessageHash, uint8(issuerSignature[64]), bytes32(issuerSignature), bytes32(issuerSignature[32]));
+
+        // Check if the recovered address matches the extracted address from the public key
+        require(recoveredAddress == issuerAddress, "Invalid signature");
 
         _mint(msg.sender, issuer.nftTokenId);
     }
-
 }
